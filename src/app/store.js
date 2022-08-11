0945b91cb7e8212b60a55f6f4033cc101cdae3e8
Emerson Warhman <edwarhman@gmail.com>
2022-08-11 18:04:01 -0400

    feat: add profile route page

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import loginReducer from '../components/loginSlice'
+import profileReducer from '../components/profileSlice'
 
 export const store = configureStore({
   reducer: {
     login: loginReducer,
+    profile: profileReducer,
   },
 })
