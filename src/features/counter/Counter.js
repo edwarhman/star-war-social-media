6f15a451d9ff6218054b7fafc46c76a1cb3adeba
Emerson Warhman <edwarhman@gmail.com>
2022-08-11 01:21:25 -0400

    build: set precommit hook

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import {
   decrement,
   increment,
@@ -7,15 +7,15 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+} from './counterSlice'
+import styles from './Counter.module.css'
 
 export function Counter() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const count = useSelector(selectCount)
+  const dispatch = useDispatch()
+  const [incrementAmount, setIncrementAmount] = useState('2')
 
-  const incrementValue = Number(incrementAmount) || 0;
+  const incrementValue = Number(incrementAmount) || 0
 
   return (
     <div>
@@ -63,5 +63,5 @@ export function Counter() {
         </button>
       </div>
     </div>
-  );
+  )
 }
